0b06f03225c1ca1efde1d7d2fe24195dd035d63f
Dana Bottoni <danabottoni@gmail.com>
2021-09-27 15:34:34 -0400

    offline functionality issue in progress

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,90 +1,63 @@
-const APP_PREFIX = 'BudgetTracker-';     
+const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-const DATA_CACHE_NAME = "data-cache-" + VERSION;
-
 const FILES_TO_CACHE = [
-  "./index.html",
-  "./manifest.json",
-  "./css/style.css",
-  "./js/index.js",
-  "./js/idb.js",
-  "./icons/icon-192x192.png",
-  "./icons/icon-512x512.png"
+    './index.html',
+    './manifest.json',
+    './css/style.css',
+    './js/index.js',
+    './js/idb.js',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192.192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512-512.png',
 ];
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
-    })
-  )
-})
-
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      let cacheKeeplist = keyList.filter(function(key) {
-        return key.indexOf(APP_PREFIX);
-      });
-      cacheKeeplist.push(CACHE_NAME);
-
-      return Promise.all(
-        keyList.map(function(key, i) {
-          if (cacheKeeplist.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i]);
-            return caches.delete(keyList[i]);
-          }
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache: ' + CACHE_NAME )
+            return cache.addAll(FILES_TO_CACHE)
         })
-      );
-    })
-  );
+    )
 });
 
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { 
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {              
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-    })
-  )
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            })
+            );
+        })
+    );
 });
 
-// self.addEventListener('fetch', function (e) {
-//   if (e.request.url.includes('/api/')){
-//     e.respondWith(
-//       caches.open(DATA_CACHE_NAME).then(cache => {
-//         return fetch (e.request)
-//         .then(response => {
-//           if (response.status === 200) {
-//             cache.put(e.request.url, response.clone())
-//           }
-//           return response;
-//         })
-//         .catch(err => {
-//           return cache.match(e.request)
-//         })
+self.addEventListener('fetch', function (event) {
+    console.log('fetch request : ' + event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + event.request.url);
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + event.request.url);
+                return fetch(event.request)
+            }
 
-//       }).catch(err => console.log(err))
-//     )
-//     return;
-//   }
-//   e.respondWith(
-//     fetch(e.request).catch(function() {
-//       return caches.match(e.request).then(function(response){
-//         if (response){
-//           return response
-//         } else if (e.request.headers.get('accept').includes('text/html')) {
-//           return caches.match('/')
-//         }
-//       })
-//     })
-//   )
-//})
+        })
+    )
+});
+  
